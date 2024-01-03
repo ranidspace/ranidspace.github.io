8b1a0b75f1a2593575b7dd010349a7dc369f9bee
Audrey <leah.gottardi@gmail.com>
2024-01-03 01:48:55 -0500

    code reformatting, added amiibo toggle, failed attempt to optimize

--- GearCheckData/gearchecker.js
+++ GearCheckData/gearchecker.js
@@ -1,18 +1,17 @@
-var output = ""
 function submit(){
   //clear all data on the page
   document.getElementById("output").innerHTML = "";
+  const noamiibo = document.getElementById("amiibo").checked;
 
   //grab data
-  let gearfile = document.getElementById('geardata').files[0];
-  console.log(gearfile);
+  const gearfile = document.getElementById('geardata').files[0];
   //read data
-  let reader = new FileReader();
+  const reader = new FileReader();
   reader.readAsText(gearfile);
 
   //do things with the data
   reader.onload = async function() {
-    var result = JSON.parse(reader.result);
+    const result = JSON.parse(reader.result);
 
     //prepping html stuff
     div = document.createElement("h1");
@@ -25,13 +24,14 @@ function submit(){
       document.getElementById("output").appendChild(div);
     
     //run the missing gear function
-    await missinggear(result, 'head', "Head")
-    await missinggear(result, 'clothing', "Clothes")
-    await missinggear(result, 'shoes', "Shoes")
+    await missinggear(result, 'head', "Head", noamiibo)
+    await missinggear(result, 'clothing', "Clothes", noamiibo)
+    await missinggear(result, 'shoes', "Shoes", noamiibo)
 
+    //Loop through 0-5 stars and find the gear for each
     for (let i = 0; i < 6; i++){
       div = document.createElement("h1");
-      div.textContent = (`${i} Star Gear`);
+      div.textContent = (i+` Star Gear`);
       document.getElementById("output").appendChild(div);
       
       div = document.createElement("div");
@@ -47,14 +47,36 @@ function submit(){
 
 }
 
-//this function just shows the missing gear for 
-async function missinggear(gearlist, Gear, GearInfo) {
-  var path = gearlist['gear']['data'][Gear+"Gears"]['nodes'];
-  var jsonlist = await jfetch(`https://raw.githubusercontent.com/Leanny/splat3/main/data/mush/600/GearInfo${GearInfo}.json`);
-  jsonlist = jsonlist.filter(x => !(x["HowToGet"] === "Impossible" || x["__RowId"] === "Shs_SHT012" || x["__RowId"] === "Shs_SHI011"|| x["__RowId"] === "Clt_HAP001"|| x["__RowId"] === "Clt_TES030" || x["__RowId"] === "Clt_TNK003"));
-  var translate = await jfetch(`https://raw.githubusercontent.com/Leanny/splat3/main/data/language/USen.json`);
+//this function shows the missing gear for 
+async function missinggear(gearlist, Gear, GearInfo, noamiibo) {
+  const path = gearlist['gear']['data'][Gear+"Gears"]['nodes'];
+  const jsonlistunfiltered = await jfetch(`https://raw.githubusercontent.com/Leanny/splat3/main/data/mush/600/GearInfo${GearInfo}.json`);
+  const translate = await jfetch(`https://raw.githubusercontent.com/Leanny/splat3/main/data/language/USen.json`);
 
-  let difference = jsonlist.filter(entry1 => !path.some(entry2 => entry1.Id === entry2[Gear+"GearId"]));
+  let jsonlist = [];
+  //TODO: improve performace. apparently for loops are faster than filtering. idk, nothing seems to work
+  if (noamiibo === true) {
+    jsonlist = jsonlistunfiltered.filter(x => 
+      !(x.HowToGet === "Impossible"||
+      x.__RowId.substr(4,3) === "AMB" ||
+      x.__RowId === "Shs_SHT012" ||
+      x.__RowId === "Shs_SHI011"||
+      x.__RowId === "Clt_HAP001"||
+      x.__RowId === "Clt_TES030" ||
+      x.__RowId === "Clt_TNK003"));
+    }
+  else {
+    jsonlist = jsonlistunfiltered.filter(x => 
+      !((x.HowToGet === "Impossible" ||
+      x.__RowId === "Shs_SHT012" || 
+      x.__RowId === "Shs_SHI011"|| 
+      x.__RowId === "Clt_HAP001"|| 
+      x.__RowId === "Clt_TES030" || 
+      x.__RowId === "Clt_TNK003"
+      )));
+  };
+
+  const difference = jsonlist.filter(entry1 => !path.some(entry2 => entry1.Id === entry2[Gear+"GearId"]));
 
   for (i=0; i < difference.length; i++) {
     div = document.createElement("div");
@@ -67,42 +89,40 @@ async function missinggear(gearlist, Gear, GearInfo) {
     div.alt = translate["CommonMsg/Gear/GearName_"+GearInfo][difference[i]["__RowId"].substr(4,)];
     document.getElementById(Gear+String(i)).appendChild(div);
 
-    div = document.createElement("p");
+    div = document.createElement("span");
       div.className = "gearname";
       div.textContent = (translate["CommonMsg/Gear/GearName_"+GearInfo][difference[i]["__RowId"].substr(4,)]);
-      document.getElementById(Gear+String(i)).appendChild(div);
+      document.getElementById(Gear+i).appendChild(div);
   }
   
 }
-
+//This function shows gear for a certain star count
 async function starcount(gearlist, Gear, GearInfo, Stars) {
-  var path = gearlist.gear.data[Gear+"s"].nodes;
-  var jsonlist = await jfetch(`https://raw.githubusercontent.com/Leanny/splat3/main/data/mush/600/${GearInfo}.json`);
+  const path = gearlist.gear.data[Gear+"s"].nodes;
+  const jsonlist = await jfetch(`https://raw.githubusercontent.com/Leanny/splat3/main/data/mush/600/${GearInfo}.json`);
 
   for (let i = 0; i < path.length; i++){
     if (path[i].rarity === Stars) {
       div = document.createElement("div");
       div.className = "item";
-      div.id = String(`${path[i].name}`)
-      document.getElementById(String(Stars)).appendChild(div);
-      
-      var gearitem = jsonlist.filter(obj => obj.Id === path[i][Gear+"Id"])
+      div.id = path[i].name;
+      document.getElementById(Stars).appendChild(div);
+
+      const gearitem = jsonlist[jsonlist.findIndex(obj => obj.Id === path[i][Gear+"Id"])]
 
       div = document.createElement("img");
-      div.src = `https://raw.githubusercontent.com/Leanny/splat3/main/images/gear/${gearitem[0]["__RowId"]}.png`;
+      div.src = `https://raw.githubusercontent.com/Leanny/splat3/main/images/gear/${gearitem.__RowId}.png`;
       div.alt = path[i].name;
-      document.getElementById(String(`${path[i].name}`)).appendChild(div);
+      document.getElementById(path[i].name).appendChild(div);
 
       div = document.createElement("span");
       div.className = "gearname";
-      div.textContent = (path[i].name);
-      document.getElementById(`${path[i].name}`).appendChild(div);
+      div.textContent = path[i].name;
+      document.getElementById(path[i].name).appendChild(div);
     }
     
   }
 }
-
-
 
 async function jfetch(url) {
   const response = await fetch(url);
@@ -111,20 +131,8 @@ async function jfetch(url) {
 
 
 //all of this is for the darkmode toggle
-function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
-  if (localStorageTheme !== null) {
-    return localStorageTheme;
-  }
-
-  if (systemSettingDark.matches) {
-    return "dark";
-  }
-
-  return "light";
-}
-
 function changetheme() {
-  var currentThemeSetting = document.querySelector("html").getAttribute("data-theme");
+  const currentThemeSetting = document.querySelector("html").getAttribute("data-theme");
   const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
   
   localStorage.setItem("theme", newTheme);
@@ -132,6 +140,7 @@ function changetheme() {
 
   currentThemeSetting = newTheme;
 }; 
+
 function loadpage() {
   switch (localStorage.getItem("theme")) {
     case null:
